29bb38a1ba788186f510ec69ca5bc6ac73fba0f7
Fabian Gutierrez <fega.hg@gmail.com>
2021-05-03 12:34:45 -0500

    renamed extension

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Export all ts
 
 Adds a export for all files in current dir.
+
+```typescript
+export * from './CanvasViewSettings';
+export * from './Shape';
+export * from './ShapeBox';
+export * from './ShapeColor';
+export * from './ShapeColorSchemaTheme';
+export * from './ShapeColorSchemaThemeColor';
+export * from './ShapeElement';
+export * from './ShapeElementBreakpoint';
+export * from './ShapeElementBreakpointStateProperties';
+export * from './ShapeElementNode';
+export * from './ShapeElementState';
+export * from './ShapeElementsCategory';
+export * from './ShapePage';
+```
--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.exportIndex', () => {
+    let disposable = vscode.commands.registerCommand('extension.exportAllTypescript', () => {
         const currentFileDir = path.dirname(vscode.window.activeTextEditor.document.uri.fsPath);
         const files = fs.readdirSync(currentFileDir)
             .filter(file => !/^index[.]\w+$/i.test(file))
